fd5969cd49bff64b65e1a3b330d2fd5c16891658
Aaron Chan <thatnerd2@gmail.com>
2015-03-28 09:28:04 -0400

    Runs

--- CrazyHair/src/com/crazyhair/MainActivity.java
+++ CrazyHair/src/com/crazyhair/MainActivity.java
@@ -1,36 +1,44 @@
 package com.crazyhair;
 
-import com.freakyhair.R;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+public class MainActivity extends SimpleBaseGameActivity {
+	 
+    static final int CAMERA_WIDTH = 1080;
+    static final int CAMERA_HEIGHT = 1920;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    }
+    
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        Camera mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED,
+            new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
+    }
+ 
+    @Override
+    protected void onCreateResources() {
+        // TODO Auto-generated method stub
+    }
+ 
+    @Override
+    protected Scene onCreateScene() {
+        Scene scene = new Scene();
+        scene.setBackground(new Background(1f, 1f, 1f));
+        VertexBufferObjectManager vertBuff = this.getVertexBufferObjectManager();
+        
+        return scene;
+    }
 }
